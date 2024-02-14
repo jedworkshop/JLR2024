a5aa9ea479460596e6c6e3b8e49380c5f197122c
s10018 <mai-om@ninjal.ac.jp>
2024-02-15 03:13:46 +0900

    move some code

--- .eleventy.js
+++ .eleventy.js
@@ -15,7 +15,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addWatchTarget("./src/materials");
 
 
-    const markdownLib = markdownIt({html: true}).use(markdownItAnchor, {
+    const markdownLib = markdownIt({html: true, highlight: function (str, lang) {return ''}}).use(markdownItAnchor, {
         permalink: markdownItAnchor.permalink.linkInsideHeader({
             symbol: '<span class="heading-anchor" aria-hidden="true">¶</span>',
             placement: 'after'
@@ -23,11 +23,32 @@ module.exports = function(eleventyConfig) {
     });
     eleventyConfig.setLibrary("md", markdownLib);
 
+    eleventyConfig.addFilter("sortDataByTime", (obj) => {
+        const sorted = {};
+        Object.keys(obj)
+          .sort((a, b) => {
+                let atime = obj[a].time.split("-")[0].split(":").map(Number)
+                let btime = obj[b].time.split("-")[0].split(":").map(Number)
+                return new Date(2024, 2, 15, atime[0], atime[1], 0) > new Date(2024, 2, 15, btime[0], btime[1], 0) ? 1 : -1;
+           }).forEach((name) => (sorted[name] = obj[name]));
+        return sorted;
+    });
+    eleventyConfig.addFilter("countSession", (obj) => {
+        if ( obj.panelists || obj.talks.length == 0 ) { return ""; }
+        var count = {"lt": 0, "normal": 0};
+        obj.talks.forEach((talk) => {
+            count[talk.type] += 1;
+        });
+        return "[" + `一般発表${count["normal"]}件` + ((count["lt"] > 0) ? `・LT${count["lt"]}件` : "") + "]";
+    });
+    eleventyConfig.addFilter("html", (obj) => { console.log(obj); return obj });
+
     return {
         dir: {
             input: "src",
             output: "docs"
         },
-        pathPrefix: "/JLR2024/"
+        pathPrefix: "/JLR2024/",
+        markdownTemplateEngine: "njk"
     }
 }
